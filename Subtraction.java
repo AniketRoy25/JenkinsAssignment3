794dc39d777b91adcf63cd5467c7cfec8ca49f0a
Aniket <aniketroy97@outlook.com>
2024-01-20 07:55:10 +0000

    Subtraction file

---
+++ Subtraction.java
@@ -0,0 +1,14 @@
+class Subtraction {
+	
+	public int add(int a, int b) {
+		return a-b;
+	}
+	
+	public static void main(String[] args) {
+		int a = 15;
+		int b = 5;
+		int c = a-b;
+		System.out.println("Subtraction is : " + c);
+	}
+
+}
